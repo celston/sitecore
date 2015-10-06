26c2135cf8a2db8fc9c60be251b01bd2ed47a1d4
Chris Elston <celston@gmail.com>
2015-10-05 21:19:38 -0500

    remove Glass bindings...until further notice

--- src/Sitecore.GnosisToolkit/Areas/GnosisToolkit/Controllers/GeneralController.cs
+++ src/Sitecore.GnosisToolkit/Areas/GnosisToolkit/Controllers/GeneralController.cs
@@ -2,7 +2,6 @@
 using System.Web.Mvc;
 
 using Sitecore.Mvc.Presentation;
-using Glass.Mapper.Sc;
 
 using Sitecore.GnosisToolkit.Areas.GnosisToolkit.Models.General;
 using Sitecore.GnosisToolkit.Library.Mvc.Controllers;
@@ -16,15 +15,6 @@ namespace Sitecore.GnosisToolkit.Areas.GnosisToolkit.Controllers
             CallToActionModel model = new CallToActionModel();
 
             InitializeModel(model);
-
-            return View(model);
-        }
-
-        public ActionResult CallToActionGlass()
-        {
-            SitecoreContext sitecoreContext = new SitecoreContext();
-
-            CallToActionGlassModel model = sitecoreContext.GetItem<CallToActionGlassModel>(RenderingContext.Current.Rendering.Item.ID.ToGuid());
 
             return View(model);
         }
