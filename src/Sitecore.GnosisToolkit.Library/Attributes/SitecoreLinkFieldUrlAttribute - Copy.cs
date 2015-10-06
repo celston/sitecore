81da342c2da53eeb01edfce140ecfee76a7080a9
Chris Elston <celston@gmail.com>
2015-10-05 21:05:05 -0500

    last set of changes before merge

---
+++ src/Sitecore.GnosisToolkit.Library/Attributes/SitecoreLinkFieldUrlAttribute - Copy.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Data.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.GnosisToolkit.Library.Attributes
+{
+    public class SitecoreLinkFieldTargetAttribute : SitecoreDataAttribute
+    {
+        public string FieldName { get; private set; }
+
+        #region Constructors
+
+        public SitecoreLinkFieldTargetAttribute()
+        {
+        }
+
+        public SitecoreLinkFieldTargetAttribute(string fieldName)
+        {
+            FieldName = fieldName;
+        }
+
+        #endregion
+
+        public override object GetValue(SitecoreFieldNamePrefixAttribute fieldNamePrefixAttribute, System.Reflection.PropertyInfo pi, Sitecore.Mvc.Presentation.Rendering rendering)
+        {
+            string fieldName = ResolveFieldName(fieldNamePrefixAttribute, pi, FieldName);
+
+            LinkField field = fieldsHelper.GetLinkField(rendering.Item, fieldName);
+
+            return field.Target;
+        }
+    }
+}
